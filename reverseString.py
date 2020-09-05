785d4e957fef6842e5737fb50279c4df870124b1
ghostshelltaken <chaitanyadodiya65@gmai.com>
2020-09-05 16:28:18 +0530

    code added

--- reverseString.py
+++ reverseString.py
@@ -4,3 +4,26 @@ Problem: Given the inut string, return the string in reverse order.
 input: "Rahul is a boy"
 output: "boy a is Rahul"
 """
+
+
+def reverseString(name):
+    result = ""
+    i, j = len(name), len(name)
+    
+    while i >= 0:
+        if name[i-1] == " ":
+            result = result + name[i:j] + " "
+            # print(i, j)
+            j = i - 1
+            i = i - 1
+        elif i == 0:
+            result = result + name[i:j]
+            j = i - 1
+            i = i - 1
+        else:
+            i = i - 1
+
+    print(result)
+    return result
+
+print(reverseString("Rahul is a boy") == "boy a is Rahul")
